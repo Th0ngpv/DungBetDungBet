5c25753dd8d3369653916c43055f49696dd05e2f
th0g.pv <thongpv160605@gmail.com>
2026-03-30 04:48:32 +0700

    feat: add logo and facebook page link

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,35 @@
 import './App.css'
 import Game from './components/Game.tsx';
+import logo from './assets/images/colorlogo.png';
+import avatar from './assets/images/avatar.png';
 
 function App() {
 
   return (
-    <>
-    
-    {/* header */}
-    <header>
-      <h1>Roulette Wheel</h1>
-    </header>
+    <body>
+        {/* header */}
+      <header>
+        <img src={logo} alt="Ngừng Bet Dừng Bết Logo" />
+      </header>
 
-    {/* main */}
-    <main>
-      {/* roulette Game */}
-      <Game />
-    </main>
-    {/* footer */}
-    <footer>
-      <p>© 2024 Roulette Game. All rights reserved.</p>
-    </footer>
-        
-    </>
+      {/* main */}
+      <main>
+        {/* roulette Game */}
+        <Game />
+      </main>
+      {/* footer */}
+      <footer>
+        <p>© 2026 Ngưng Bet Dừng Bết.</p>
+      </footer>
+      <a
+          href="https://www.facebook.com/profile.php?id=61588305654233"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="fb-avatar"
+        >
+          <img src={avatar} alt="Facebook" />
+        </a>
+    </body>
   )  
 }
 
